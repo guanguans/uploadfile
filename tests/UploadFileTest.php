97d39e6b62989d80c24682d58126365763e01772
琯琯 <53222411@qq.com>
2018-08-08 17:52:15 +0800

    test: 添加 getErrorMsg 测试

--- tests/UploadFileTest.php
+++ tests/UploadFileTest.php
@@ -3,8 +3,19 @@
 namespace Guanguans\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Guanguans\UploadFile;
 
 class UploadFileTest extends TestCase
 {
+    protected $UploadFile;
 
+    public function setUp()
+    {
+        $this->UploadFile = new UploadFile();
+    }
+
+    public function testGetErrorMsg()
+    {
+        $this->assertNotEmpty('this is a test');
+    }
 }
